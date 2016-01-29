ddf1c9bbd7f3bb8179f3cf7c13ee8132e9dfb005
epsalt <evan.saltman@gmail.com>
2016-01-29 00:23:51 -0700

    clean up routing, change button centering

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from werkzeug import secure_filename
-from PIL import Image
-import PIL.ImageOps
+from sketch2model.segment_5 import sketch2model
 
 UPLOAD_FOLDER = './assets/'
 SKETCH_FOLDER = './sketches/'
@@ -20,37 +19,49 @@ def allowed_file(filename):
 def index():
     print(request.method)
     if request.method == 'POST':
-        file = request.files.get('uploaded_file')
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return(redirect(url_for('uploaded', filename=filename)))
+        print("UPLOADING")
+        filename = upload()
+        return(redirect(url_for('uploaded', filename=filename)))
     else:
         return(render_template("app.html"))
+    
+def upload():
+    file = request.files.get('uploaded_file')
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return(filename)
+    
+@app.route("/uploaded/<filename>", methods=['GET', 'POST'])
+def uploaded(filename):
+    if request.method == 'GET':
+        if request.args.get('sketch_button'):
+            return(sketch(filename))
+        else:
+            return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename)))
+    elif request.method == 'POST':
+        return(redirect(url_for('index'), code=307))
+        
+def sketch(filename):
+    print(filename)
+    #--- Image processing goes here ---
+    image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    inverted_image = PIL.ImageOps.invert(image)
+    inverted_image.save(os.path.join(app.config['SKETCH_FOLDER'], filename))
+    #--- ---
+    return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename),
+                           sketched_image=url_for('sketched_file', filename=filename)))
 
 @app.route("/uploads/<filename>")
 def uploaded_file(filename):
      return(send_from_directory(app.config['UPLOAD_FOLDER'],
                                 filename))
-
+ 
 @app.route("/sketches/<filename>")
 def sketched_file(filename):
     return(send_from_directory(app.config['SKETCH_FOLDER'],
                                filename))
 
-@app.route("/uploaded/<filename>", methods=['GET', 'POST'])
-def uploaded(filename):
-    if request.method == 'GET' and request.args.get('sketch_button'):
-        # Image processing goes here
-        image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        inverted_image = PIL.ImageOps.invert(image)
-        inverted_image.save(os.path.join(app.config['SKETCH_FOLDER'], filename))
-
-        # Render template with original image and sketch
-        return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename),
-                               sketched_image=url_for('sketched_file', filename=filename)))
-    return(render_template("app.html",uploaded_image=url_for('uploaded_file', filename=filename)))
-    
 @app.route("/about")
 def about():
     return(render_template("about.html"))
