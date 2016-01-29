d16cb4cb1fa2daf25ebfc536d9422d4781c12774
epsalt <evan.saltman@gmail.com>
2016-01-29 16:52:26 -0700

    changed app to application for eb

--- application.py
+++ application.py
@@ -8,15 +8,15 @@ UPLOAD_FOLDER = './assets/'
 SKETCH_FOLDER = './sketches/'
 ALLOWED_EXTENSIONS = set(['jpg', 'png'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SKETCH_FOLDER'] = SKETCH_FOLDER
+application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+application.config['SKETCH_FOLDER'] = SKETCH_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
+@application.route("/", methods=['GET', 'POST'])
 def index():
     print(request.method)
     if request.method == 'POST':
@@ -24,46 +24,46 @@ def index():
         filename = upload()
         return(redirect(url_for('uploaded', filename=filename)))
     else:
-        return(render_template("app.html"))
+        return(render_template("application.html"))
     
 def upload():
     file = request.files.get('uploaded_file')
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
         return(filename)
     
-@app.route("/uploaded/<filename>", methods=['GET', 'POST'])
+@application.route("/uploaded/<filename>", methods=['GET', 'POST'])
 def uploaded(filename):
     if request.method == 'GET':
         if request.args.get('sketch_button'):
             return(sketch(filename))
         else:
-            return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename)))
+            return(render_template("application.html", uploaded_image=url_for('uploaded_file', filename=filename)))
     elif request.method == 'POST':
         return(redirect(url_for('index'), code=307))
         
 def sketch(filename):
-    sketch_name = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    model_name = os.path.join(app.config['SKETCH_FOLDER'], filename)
+    sketch_name = os.path.join(application.config['UPLOAD_FOLDER'], filename)
+    model_name = os.path.join(application.config['SKETCH_FOLDER'], filename)
     sketch2model(sketch_name, model_name)
     #--- ---
-    return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename),
+    return(render_template("application.html", uploaded_image=url_for('uploaded_file', filename=filename),
                            sketched_image=url_for('sketched_file', filename=filename)))
 
-@app.route("/uploads/<filename>")
+@application.route("/uploads/<filename>")
 def uploaded_file(filename):
-     return(send_from_directory(app.config['UPLOAD_FOLDER'],
+     return(send_from_directory(application.config['UPLOAD_FOLDER'],
                                 filename))
  
-@app.route("/sketches/<filename>")
+@application.route("/sketches/<filename>")
 def sketched_file(filename):
-    return(send_from_directory(app.config['SKETCH_FOLDER'],
+    return(send_from_directory(application.config['SKETCH_FOLDER'],
                                filename))
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return(render_template("about.html"))
 
 if __name__ == "__main__":
-      app.run()
+      application.run()
