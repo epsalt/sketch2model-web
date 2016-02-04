f084829f77557494d4929edc2b97e7f72fcbe8ce
epsalt <evan.saltman@gmail.com>
2016-02-03 22:40:27 -0700

    get rid of globals being used in helper functions, group routing and helpers together

--- application.py
+++ application.py
@@ -5,30 +5,18 @@ from flask import Flask, render_template, request, send_from_directory, redirect
 from werkzeug import secure_filename
 from sketch2model.segment_5 import sketch2model
 
+app = Flask(__name__)
+app.config['S3_BUCKET'] = 'sketch2model'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SKETCH_FOLDER'] = 'sketches'
 ALLOWED_EXTENSIONS = set(['jpg', 'png'])
-S3_BUCKET = 'sketch2model'
-S3_UPLOAD_FOLDER = 'uploads'
-S3_SKETCH_FOLDER = 'sketch'
 
-application = Flask(__name__)
-application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-application.config['SKETCH_FOLDER'] = SKETCH_FOLDER
-
+## Helper functions
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@application.route("/", methods=['GET', 'POST'])
-def index():
-    print(request.method)
-    if request.method == 'POST':
-        print("UPLOADING")
-        return(url)
-        return(redirect(url_for('uploaded', filename=filename)))
-    else:
-        return(render_template("app.html"))
-    
-def upload():
+def upload(bucket, folder):
     file = request.files.get('uploaded_file')
     if file and allowed_file(file.filename):
         # Strip extension off upload and generate new filename
@@ -38,51 +26,60 @@ def upload():
 
         # Upload image to s3
         s3 = boto3.resource('s3')
-        s3.Object(S3_BUCKET, S3_UPLOAD_FOLDER + '/'
-                  + upload_filename).put(Body=file,
-                                         ContentType='image/jpeg',
-                                         ACL='public-read')
+        s3.Object(bucket, folder + '/'+
+                  upload_filename).put(Body=file,
+                                       ContentType='image/jpeg',
+                                       ACL='public-read')
         # Generate URL
         url = "https://{bucket}.s3.amazonaws.com/{folder}/{fname}".format(
-            bucket = S3_BUCKET,
-            folder = S3_UPLOAD_FOLDER,
+            bucket = bucket,
+            folder = folder,
             fname  = upload_filename)
         return(url)
 
+def sketch(filename):
+    sketch_name = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    model_name = os.path.join(app.config['SKETCH_FOLDER'], filename)
+    sketch2model(sketch_name, model_name)
+    return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename),
+                           sketched_image=url_for('sketched_file', filename=filename)))
+
 def generate_filename():
     return(str(round(time.time(), 1)).replace(".", ""))
-    
-@application.route("/uploaded/<filename>", methods=['GET', 'POST'])
+
+## Routing
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = upload(app.config['S3_BUCKET'], app.config['UPLOAD_FOLDER'])
+        return(url)
+        return(redirect(url_for('uploaded', filename=filename)))
+    else:
+        return(render_template("app.html"))
+
+@app.route("/uploaded/<filename>", methods=['GET', 'POST'])
 def uploaded(filename):
     if request.method == 'GET':
         if request.args.get('sketch_button'):
             return(sketch(filename))
         else:
-            return(render_template("application.html", uploaded_image=url_for('uploaded_file', filename=filename)))
+            return(render_template("app.html", uploaded_image=url_for('uploaded_file', filename=filename)))
     elif request.method == 'POST':
         return(redirect(url_for('index'), code=307))
         
-def sketch(filename):
-    sketch_name = os.path.join(application.config['UPLOAD_FOLDER'], filename)
-    model_name = os.path.join(application.config['SKETCH_FOLDER'], filename)
-    sketch2model(sketch_name, model_name)
-    #--- ---
-    return(render_template("application.html", uploaded_image=url_for('uploaded_file', filename=filename),
-                           sketched_image=url_for('sketched_file', filename=filename)))
-
-@application.route("/uploads/<filename>")
+@app.route("/uploads/<filename>")
 def uploaded_file(filename):
-     retuasdfrn(send_from_directory(application.config['UPLOAD_FOLDER'],
+     retuasdfrn(send_from_directory(app.config['UPLOAD_FOLDER'],
                                 filename))
  
-@application.route("/sketches/<filename>")
+@app.route("/sketches/<filename>")
 def sketched_file(filename):
-    return(send_from_directory(application.config['SKETCH_FOLDER'],
+    return(send_from_directory(app.config['SKETCH_FOLDER'],
                                filename))
 
-@application.route("/about")
+@app.route("/about")
 def about():
     return(render_template("about.html"))
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
