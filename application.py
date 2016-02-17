97db76d71840a404245113eb8ec5d2cd5966acdc
epsalt <evan.saltman@gmail.com>
2016-02-17 02:20:49 -0700

    added some docstrings

--- application.py
+++ application.py
@@ -13,10 +13,15 @@ ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'gif', 'png'])
 
 ## Helper functions
 def allowed_file(filename):
+    """Checks if a filename is in the set of allowed extensions specified
+    in the app.config object."""
+
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 def upload(bucket, folder):
+    """Validates user file from POST request and uploads to S3."""
+
     file = request.files.get('uploaded_file')
     if file and allowed_file(file.filename):
         
@@ -30,27 +35,34 @@ def upload(bucket, folder):
         return(upload_filename)
 
 def s3_put(file, filename, bucket, folder):
+    """Convenience wrapper function for object upload to S3."""
     s3 = resource('s3')
     s3.Object(bucket, folder + '/' + filename).put(Body=file,
                                                    ContentType='image/jpeg')
 
 def s3_get(filename, bucket, folder):
+    """Convenience wrapper function for object download from S3."""
     s3 = resource('s3')
     return(s3.Object(bucket, folder + '/' + filename).get()['Body'])
 
 def s3_url(filename, bucket, folder):
+    """Builds a S3 object URL from components."""
     return("https://{bucket}.s3.amazonaws.com/{folder}/{filename}".format(
         bucket  = bucket,
         folder  = folder,
         filename = filename))
 
 def model(filename, bucket, upload_folder, model_folder):
+    """Pulls sketch image from S3, runs sketch2model image processing 
+    on the image and then uploads result to S3."""
     sketch = s3_get(filename, bucket, upload_folder)
     model_object = sketch2model(sketch, filename)
     s3_put(model_object, filename, bucket, model_folder)
     model_object.close()
 
 def generate_filename():
+    """Generates a storage filename based on system time. Filenames
+    are not random. Names match for both sketch and model."""
     return(str(round(time.time(), 1)).replace(".", ""))
 
 ## Routing
