39083fe440082eb1de45d63e96101a260a1eff62
epsalt <evan.saltman@gmail.com>
2016-03-01 16:29:35 -0700

    fix import on error view and add 404

--- sketch2model_web/__init__.py
+++ sketch2model_web/__init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-from sketch2model_web.views import general
+from sketch2model_web.views import index
 from sketch2model_web.views import errors
--- sketch2model_web/views/errors.py
+++ sketch2model_web/views/errors.py
@@ -1,4 +1,9 @@
 from sketch2model_web import app
+from flask import render_template
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(error):
