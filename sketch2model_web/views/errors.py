ca1610691f666aa4559fded82bc8142ec75dde80
epsalt <evan.saltman@gmail.com>
2017-01-12 19:51:13 -0700

    Redo 404 page

--- sketch2model_web/views/errors.py
+++ sketch2model_web/views/errors.py
@@ -1,16 +1,7 @@
-from sketch2model_web import app
 from flask import render_template
+
+from sketch2model_web import app
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    app.logger.error('Server Error: %s', error)
-    return render_template('500.html'), 500
-
-@app.errorhandler(Exception)
-def unhandled_exception(e):
-    app.logger.error('Unhandled Exception: %s', e)
-    return render_template('500.html'), 500
