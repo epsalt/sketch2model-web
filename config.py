678256fd75e6637ae2c890c4498d668923c99fb1
epsalt <evan.saltman@gmail.com>
2017-08-29 22:06:00 -0600

    Downsize large images before processing

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ S3_BUCKET = 'sketch2model'
 UPLOAD_FOLDER = 'uploads'
 MODEL_FOLDER = 'models'
 EXAMPLE_FOLDER = 'examples'
+MAX_IMAGE_SIZE = 800
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'gif', 'png'])
--- sketch2model_web/utils.py
+++ sketch2model_web/utils.py
@@ -1,12 +1,14 @@
 import requests
 import io
-import PIL
 import time
+from PIL import Image
 from boto3 import resource
 from matplotlib import cm, colors
 
 from sketch2model_web import app
 
+max_width = app.config['MAX_IMAGE_SIZE']
+max_height = app.config['MAX_IMAGE_SIZE']
 bucket = app.config['S3_BUCKET']
 upload_folder = app.config['UPLOAD_FOLDER']
 model_folder = app.config['MODEL_FOLDER']
@@ -21,6 +23,17 @@ def s3_url(fname):
 
 def generate_filename(): return str(round(time.time(), 1)).replace(".", "")
 
+def resize(im):
+    im = Image.open(im)
+    resize_ratio = min(max_width/im.width, max_height/im.height, 1)
+    new_width = round(resize_ratio*im.width)
+    new_height = round(resize_ratio*im.height)
+    im = im.resize((new_width, new_height), Image.ANTIALIAS)
+    buf = io.BytesIO()
+    im.save(buf, format="PNG")
+    buf.seek(0)
+    return(buf)
+
 def upload(f, model, ext = ".png"):
     if not model and not allowed_file(f.filename):
             raise ValueError
@@ -33,7 +46,8 @@ def upload(f, model, ext = ".png"):
 def load_img(url):
     url = requests.utils.unquote(url)
     response = requests.get(url)
-    img = PIL.Image.open(io.BytesIO(response.content))
+    buf = io.BytesIO(response.content)
+    img = Image.open(resize(buf))
     return img
 
 def array_to_img(a, cmap, format="PNG"):
@@ -58,7 +72,7 @@ def array_to_img(a, cmap, format="PNG"):
     norm_color_map = cm.ScalarMappable(norm=norm, cmap=cmap_dict.get(cmap))
 
     ## Convert image from array and save to buffer
-    im = PIL.Image.fromarray(norm_color_map.to_rgba(a, bytes=True))
+    im = Image.fromarray(norm_color_map.to_rgba(a, bytes=True))
     buf = io.BytesIO()
     im.save(buf, format=format)
     buf.seek(0)
