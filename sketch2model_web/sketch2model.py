d9e96ebea77db81ec371feb400db72803f825c16
Evan Saltman <evan.saltman@gmail.com>
2022-06-02 21:32:30 -0600

    Remove submodule

---
+++ sketch2model_web/sketch2model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import scipy as sp
+from skimage import color, exposure, morphology, measure, segmentation
+
+
+class Sketch2Model:
+    def __init__(self, im, contrast=0.5, closing=5):
+        self.initial_image = np.asarray(im)
+        self.process(contrast, closing)
+
+    def process(self, contrast, closing):
+        """Sketch2Model uses morphological filtering to label regions in a
+        hollow sketch and assign them discrete labels"""
+
+        # Compressor
+        im = color.rgb2gray(self.initial_image[0:-1:2, 0:-1:2])
+        flt2 = sp.ndimage.filters.gaussian_filter(im**2, 21)
+        self.compressed = im / (np.sqrt(flt2))
+
+        # Contrast streching
+        p2, p98 = np.percentile(self.compressed, (2, 98))
+        self.contrasted = exposure.rescale_intensity(
+            self.compressed, in_range=(p2, p98)
+        )
+
+        # Binarization with scalar threshold
+        self.contrasted_3d = np.stack((self.contrasted,) * 3, axis=-1)
+        self.binary = ~(color.rgb2gray(self.contrasted_3d) > contrast)
+
+        # Use binary closing to connect lines that aren't
+        # completely crossing
+        self.closed = morphology.binary_closing(self.binary, morphology.disk(closing))
+
+        # Remove small objects (bright and dark spots)
+        removed = morphology.remove_small_objects(self.closed)
+        self.removed = ~morphology.remove_small_objects(~removed)
+
+        # Skeletonize and dilate to get final boundaries
+        edges = morphology.skeletonize(self.removed)
+        self.skeletonized = ~morphology.dilation(edges, morphology.disk(1))
+
+        # Label regions
+        clean_border = segmentation.clear_border(self.skeletonized)
+        self.labeled, self.nregions = measure.label(
+            clean_border, background=999999, return_num=True
+        )
+        self.labeled = self.labeled + 1
+
+        # Change border region labels to zero for random walker
+        # segmentation in next step
+        np.place(self.labeled, np.logical_not(self.skeletonized), 0)  # updates in place
+
+        # Segmentation of border pixels
+        self.final = segmentation.random_walker(
+            self.skeletonized, self.labeled, beta=1, mode="cg_mg"
+        )
--- sketch2model_web/views/api.py
+++ sketch2model_web/views/api.py
@@ -1,72 +1,53 @@
 from sketch2model_web import app
 from flask import request, jsonify
 
-from sketch2model_web.sketch2model.image_processing import Sketch2Model
+from sketch2model_web.sketch2model import Sketch2Model
 from sketch2model_web.utils import load_img, upload, array_to_img, s3_url
+
 
 @app.route("/api/heartbeat")
 def api_heartbeat():
     """Health check for the API"""
-    result = {
-        "ok": True,
-        "error": ""
-    }
+    result = {"ok": True, "error": ""}
     return jsonify(result)
+
 
 @app.route("/api")
 def api_sketch2model():
 
     try:
-        url = request.args['url']
-        contrast = float(request.args['contrast'])
-        closing = int(request.args['closing'])
-        cmap = request.args['cmap']
+        url = request.args["url"]
+        contrast = float(request.args["contrast"])
+        closing = int(request.args["closing"])
+        cmap = request.args["cmap"]
 
     except Exception as e:
-        result = {
-            "ok": False,
-            "url": "",
-            "error": "url parameter not found"
-        }
+        result = {"ok": False, "url": "", "error": "url parameter not found"}
         return jsonify(result)
 
     try:
         img = load_img(url)
 
     except OSError as e:
-        result = {
-            "ok": False,
-            "url": "",
-            "error": "could not open image file"
-            }
+        result = {"ok": False, "url": "", "error": "could not open image file"}
         return jsonify(result)
 
     try:
-        model = Sketch2Model(load_img(url),
-                             contrast=contrast,
-                             closing=closing)
+        model = Sketch2Model(load_img(url), contrast=contrast, closing=closing)
 
         if model.nregions == 1:
             result = {
                 "ok": False,
                 "url": "",
-                "error": "only identified one region in input image, try tuning parameters or simplify input image"
+                "error": "only identified one region in input image, try tuning parameters or simplify input image",
             }
 
         else:
-            fname = upload(array_to_img(model.final, cmap), model = True)
+            fname = upload(array_to_img(model.final, cmap), model=True)
             url = s3_url(fname)
-            result = {
-                "ok": True,
-                "url": url.get("modeled"),
-                "error": ""
-            }
+            result = {"ok": True, "url": url.get("modeled"), "error": ""}
         return jsonify(result)
 
     except Exception as e:
-        result = {
-            "ok": False,
-            "url": "",
-            "error": str(e)
-            }
+        result = {"ok": False, "url": "", "error": str(e)}
         return jsonify(result)
